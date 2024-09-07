c7df41d815b81e64e925c990ea0a653c8a9df5cb
Dilpreet Singh <dilpreetsinghaulakh@icloud.com>
2024-09-08 00:56:20 +0530

    Refactor linkUserWithHouse function and update house data

--- apps/reside-hub/app/user/linkUserWithHouse.ts
+++ apps/reside-hub/app/user/linkUserWithHouse.ts
@@ -1,10 +1,20 @@
 import { supabase } from '../api/supabase';
 
-export default function linkUserWithHouse(house_id: string, tenant_id: string) {
+export default function linkUserWithHouse(
+  house_id: string,
+  tenant_id: string,
+  rent: number
+) {
   async function fetchData(house_id: string) {
     let { data, error } = await supabase
       .from('house')
-      .update({ tenant_id: tenant_id })
+      .update({
+        tenant_id: tenant_id,
+        lease_date: new Date().toISOString(),
+        pending_rent: 0,
+        notification: [],
+        history: [{ payment: new Date().toISOString(), rent: rent }],
+      })
       .eq('id', house_id);
 
     if (error) {
