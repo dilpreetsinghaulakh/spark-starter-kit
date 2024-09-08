e5be1d83151ea46672ee9b5b592301e407ae6448
Dilpreet Singh <dilpreetsinghaulakh@icloud.com>
2024-09-08 13:03:05 +0530

    Refactor Header component to add user id display and logout functionality

--- apps/reside-hub/app/components/Header.tsx
+++ apps/reside-hub/app/components/Header.tsx
@@ -1,21 +1,36 @@
-"use client";
+'use client';
+
+import { useEffect, useState } from 'react';
 
 export default function Header() {
+  const [userid, setUserId] = useState('');
+  useEffect(() => {
+    setUserId(sessionStorage.getItem('user_id') || '');
+
+    window.addEventListener('click', () => {
+      setUserId(sessionStorage.getItem('user_id') || '');
+    }); // This is a hack to update the user id when the user logs in
+
+    return () => {
+      setUserId('');
+    };
+  }, []);
   const handleLogout = () => {
     sessionStorage.removeItem('user_id');
     window.location.reload();
   };
   return (
     <div className="flex justify-between items-center px-8 py-4 mb-8">
-      <img src="/logo.png" alt="Logo" className="h-12 w-auto" />
-      <div className="flex gap-4">
+      <p className="text-xl">RH</p>
+
+      {userid ? (
         <button
           onClick={handleLogout}
           className="border border-red-500/50 bg-red-500/10 text-black px-6 py-2 font-semibold rounded-lg"
         >
           Logout
         </button>
-      </div>
+      ) : null}
     </div>
   );
 }
